88e59133b3a2a252441ce0dbd02313169f0482b1
Casey K <tehcnicolour@gmail.com>
2021-07-07 18:13:05 -0700

    no cd into functions

--- web/src/community/Community/styles.scss.ts
+++ web/src/community/Community/styles.scss.ts
@@ -5,28 +5,8 @@
 import styled from 'styled-components'
 
 import theme from '../../gatsby-plugin-theme-ui'
-import { Box, Flex } from '../../components/ui'
+import { Box, Flex } from 'theme-ui'
 
 // ___________________________________________________________________
 
-export const User = styled(Flex)`
-  background: ${theme.colors.black};
-
-  .inner {
-    margin-right: auto;
-    max-width: ${theme.maxWidth};
-    width: 100%;
-  }
-`
-
-export const Aside = styled(Box)`
-  background: ${theme.colors.black};
-  border-bottom: ${theme.border};
-  flex: 1;
-  position: relative;
-
-  @media ${theme.mq.tablet} {
-    border-bottom: 0;
-    border-left: ${theme.border};
-  }
-`
+export const User = styled(Flex)``
